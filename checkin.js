36b594c80b1719027f2a3ca245dab487fa6311b8
Jac <lu7766lu7766@gmail.com>
2021-07-02 22:36:23 +0800

    err

--- checkin.js
+++ checkin.js
@@ -1,41 +1,42 @@
 const puppeteer = require('puppeteer')
 
 module.exports = async (req, res) => {
-	// try {
-	const { username, password } = req.body
+	try {
+		const { username, password } = req.body
 
-	const browser = await puppeteer.launch({
-		// headless: false,
-	})
+		const browser = await puppeteer.launch({
+			// headless: false,
+		})
 
-	const page = await browser.newPage()
-	await page.goto('https://bsignin.104.com.tw/login')
-	await page.waitForSelector('.BaseInput')
-	await page.waitForSelector('.BaseInput__view')
-	await page.type('.BaseInput__view[type="text"]', username)
-	await page.type('.BaseInput__view[type="password"]', password)
-	await page.click('.BaseButton')
-	await page.waitForSelector('.Product__product')
-	await page.goto('https://pro.104.com.tw/psc2')
-	await page.waitForSelector('.btn.btn-white.btn-lg.btn-block')
+		const page = await browser.newPage()
+		await page.goto('https://bsignin.104.com.tw/login')
+		await page.waitForSelector('.BaseInput')
+		await page.waitForSelector('.BaseInput__view')
+		await page.type('.BaseInput__view[type="text"]', username)
+		await page.type('.BaseInput__view[type="password"]', password)
+		await page.click('.BaseButton')
+		await page.waitForSelector('.Product__product')
+		await page.goto('https://pro.104.com.tw/psc2')
+		await page.waitForSelector('.btn.btn-white.btn-lg.btn-block')
 
-	await page.click('.fa.fa-times')
-	await page.click('.btn.btn-white.btn-lg.btn-block')
-	// await page.screenshot({
-	// 	path: './checkin.png',
-	// 	fullPage: true,
-	// })
-	await page.waitForTimeout(1000)
+		await page.click('.fa.fa-times')
+		await page.click('.btn.btn-white.btn-lg.btn-block')
+		// await page.screenshot({
+		// 	path: './checkin.png',
+		// 	fullPage: true,
+		// })
+		await page.waitForTimeout(1000)
 
-	await browser.close()
-	res.json({
-		code: 0,
-		result: 'success',
-	})
-	// } catch (err) {
-	// 	res.json({
-	// 		code: -1,
-	// 		result: err,
-	// 	})
-	// }
+		await browser.close()
+		res.json({
+			code: 0,
+			result: 'success',
+		})
+	} catch (err) {
+		throw err
+		// res.json({
+		// 	code: -1,
+		// 	result: err,
+		// })
+	}
 }
